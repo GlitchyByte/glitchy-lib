b2c7ed66ebe5534228926f5698a9c7297506b353
Luis Mejia <lmejia@gmail.com>
2020-12-21 17:40:12 -0800

    Added javadocs.

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.glitchybyte.lib"},{"l":"com.glitchybyte.lib.console"},{"l":"com.glitchybyte.lib.log"},{"l":"com.glitchybyte.lib.mutable"}];updateSearchResults();
