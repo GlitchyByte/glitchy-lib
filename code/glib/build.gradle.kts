65936cb599594f1edf5f50fc42d331fcbd3e9fe3
Luis Mejia <lmejia@gmail.com>
2021-11-16 06:49:07 -0800

    Added GTimedCacheMap for expiring caches.

--- code/glib/build.gradle.kts
+++ code/glib/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
 
 // Setup build info.
 group = "com.glitchybyte.glib"
-version = "1.0.0"
+version = "1.0.1"
 
 tasks.named<Javadoc>("javadoc") {
     title = "${rootProject.name} v${version} API"
