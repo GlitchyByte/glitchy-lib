205d21b6c4078474e2e49898c83984d2ad64ace5
Luis Mejia <lmejia@gmail.com>
2023-08-22 06:10:42 -0700

    Removed docs. They will be generated by GitHub Actions.

--- docs/gspring/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.glitchybyte.gspring"},{"l":"com.glitchybyte.gspring.configuration"},{"l":"com.glitchybyte.gspring.endpoint"}];updateSearchResults();
